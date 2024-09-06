325f7416ece0348d140bdab41468a73551ad5c0e
Lucas Einig <einig@iram.fr>
2024-09-06 08:04:45 +0200

    Fix break_degeneracy

--- infovar/stats/preprocessing.py
+++ infovar/stats/preprocessing.py
@@ -1,21 +1,19 @@
 import numpy as np
 
-__all__ = [
-    "break_degeneracy"
-]
+__all__ = ["break_degeneracy"]
 
 
-def break_degeneracy(
-    data: np.ndarray
-) -> np.ndarray:
+def break_degeneracy(data: np.ndarray) -> np.ndarray:
     """
     Measures the sample step and add an adequate noise to break degeneracy.
-    data: np.ndarray of shape (N, dim)
+    data: np.ndarray of shape (N, dim).
+
+    Note: seems not to work when applying a logarithm.
     """
     diffs = np.diff(np.sort(data, axis=0), axis=0)
-    diffs = np.where(diffs==0, np.nan, diffs)
-    
+    diffs = np.where(diffs == 0, np.nan, diffs)
+
     steps = np.nanmin(diffs, axis=0)
-    
-    datac = (np.random.random(size=data.shape)-0.5) * steps
-    return datac
+
+    datac = 0.5 * (np.random.random(size=data.shape) - 0.5) * steps
+    return data + datac
